7ef8a7f6b80c89545be8d3b69279193df83b6fff
aldoserehd <68321828+aldoserehd@users.noreply.github.com>
2020-12-22 16:14:13 +0300

    Done with the frontend

---
+++ components/authentication/Signin.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import {
+  AuthContainer,
+  AuthTitle,
+  AuthButton,
+  AuthButtonText,
+  AuthTextInput,
+  AuthOther,
+} from "../../styles";
+import authStore from "../../stores/authStore";
+import { observer } from "mobx-react";
+const Signin = ({ navigation }) => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = async () => {
+    await authStore.signin(user);
+    if (authStore.user) navigation.replace("MaktabaList");
+  };
+  return (
+    <AuthContainer>
+      <AuthTitle>Signin</AuthTitle>
+      <AuthTextInput
+        onChangeText={(username) => setUser({ ...user, username })}
+        placeholder="Username"
+      />
+      <AuthTextInput
+        onChangeText={(password) => setUser({ ...user, password })}
+        placeholder="Password"
+        placeholderTextColor="#A6AEC1"
+        secureTextEntry={true}
+      />
+      <AuthOther onPress={() => navigation.navigate("Signup")}>
+        Click here to register!
+      </AuthOther>
+      <AuthButton onPress={handleSubmit}>
+        <AuthButtonText>Sign in</AuthButtonText>
+      </AuthButton>
+    </AuthContainer>
+  );
+};
+
+export default observer(Signin);
---
+++ components/authentication/Signup.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import {
+  AuthContainer,
+  AuthTitle,
+  AuthButton,
+  AuthButtonText,
+  AuthTextInput,
+  AuthOther,
+} from "../../styles";
+import authStore from "../../stores/authStore";
+import { observer } from "mobx-react";
+const Signup = ({ navigation }) => {
+  const [user, setUser] = useState({
+    username: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async () => {
+    await authStore.signup(user);
+    if (authStore.user) navigation.replace("MaktabaList");
+  };
+  return (
+    <AuthContainer>
+      <AuthTitle>Signup</AuthTitle>
+      <AuthTextInput
+        onChangeText={(username) => setUser({ ...user, username })}
+        placeholder="Username"
+      />
+      <AuthTextInput
+        onChangeText={(password) => setUser({ ...user, password })}
+        placeholder="Password"
+        placeholderTextColor="#A6AEC1"
+        secureTextEntry={true}
+      />
+      <AuthTextInput
+        onChangeText={(firstName) => setUser({ ...user, firstName })}
+        placeholder="First name"
+      />
+      <AuthTextInput
+        onChangeText={(lastName) => setUser({ ...user, lastName })}
+        placeholder="Last name"
+      />
+      <AuthTextInput
+        onChangeText={(email) => setUser({ ...user, email })}
+        placeholder="email"
+      />
+      <AuthOther onPress={() => navigation.navigate("Signin")}>
+        Click here to Login !
+      </AuthOther>
+      <AuthButton onPress={handleSubmit}>
+        <AuthButtonText>Sign up</AuthButtonText>
+      </AuthButton>
+    </AuthContainer>
+  );
+};
+
+export default observer(Signup);
