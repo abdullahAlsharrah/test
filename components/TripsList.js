5450b5f31f7cd1f93b1b331f33c548b68a72726d
aldoserehd <68321828+aldoserehd@users.noreply.github.com>
2020-12-24 12:41:29 +0300

    Done

--- components/TripsList.js
+++ components/TripsList.js
@@ -1,10 +1,13 @@
-import { Content, List } from "native-base";
+import { Content, List, Spinner } from "native-base";
 import React from "react";
 import TripsItem from "./TripsItem";
+import tripStore from "../stores/tripStore";
 import { observer } from "mobx-react";
-const TripList = ({ trips }) => {
-  const tripList = trips.map((trip) => <TripsItem trip={trip} key={trip.id} />);
-
+const TripList = () => {
+  if (tripStore.loading) return <Spinner />;
+  const tripList = tripStore.trips.map((trip) => (
+    <TripsItem trip={trip} key={trip.id} />
+  ));
   return (
     <Content>
       <List>{tripList}</List>
--- stores/tripStore.js
+++ stores/tripStore.js
@@ -1,19 +1,43 @@
-import { action, makeObservable, observable } from "mobx";
+import { makeAutoObservable } from "mobx";
 import instance from "./instance";
 class TripStore {
   trips = [];
+  loading = true;
   constructor() {
-    makeObservable(this, {
-      trips: observable,
-      fetchtrips: action,
-    });
+    makeAutoObservable(this);
   }
   fetchtrips = async () => {
     try {
       const response = await instance.get("/trips");
       this.trips = response.data;
+      this.loading = false;
     } catch (error) {
       console.log("error line : 16");
+    }
+  };
+
+  createTrip = async (newTrip, formData, cd) => {
+    try {
+      const res = await instance.post(`/trips`, formData);
+      cd();
+      this.trips.push(res.data);
+    } catch (error) {}
+  };
+
+  deleteTrip = async (tripId) => {
+    try {
+      await instance.delete(`/trips/${tripId}`);
+      this.trips = this.trips.filter((trip) => trip.id !== tripId);
+    } catch (error) {}
+  };
+
+  updateTrip = async (updatedTrip) => {
+    try {
+      await instance.put(`/trips/${updatedTrip.id}`, updatedTrip);
+      const trip = this.trip.find((trip) => trip.id === updatedTrip.id);
+      for (const key in trip) trip[key] = updatedTrip[key];
+    } catch (error) {
+      console.log("TripStore -> updateTrip -> error", error);
     }
   };
   getTripById = (tripId) => this.trips.find((trip) => trip.id === tripId);
